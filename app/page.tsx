860261cbadf6a9c56a2dcd57d94e7cb4fee6b140
Max <kvsam7395@gmail.com>
2025-11-25 19:09:46 +0200

    fix position

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { GlassButton } from '@/components/ui/GlassButton';
 
 export default function Home() {
     return (
-        <div className="w-full h-screen flex items-center justify-center gap-100">
+        <div className="w-full h-screen flex items-center justify-center flex-col gap-4">
             <GlassButton>SoundInfluencers</GlassButton>
             <Loading />
         </div>
